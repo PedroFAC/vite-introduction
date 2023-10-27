febef101dec232ce4afdd1cba818c04973ac4458
Pedro Façanha <pedrofacpeix@gmail.com>
2023-10-27 15:10:11 -0300

    strictPort & logLevel

--- vite-config/vite.config.js
+++ vite-config/vite.config.js
@@ -3,4 +3,16 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig({
   clearScreen: false,
   envPrefix: "APP_",
+  logLevel: "silent",
+  server: {
+    port: 3000,
+    // strictPort: true,
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  //   css: {
+  //     devSourcemap: true,
+  //   },
 });
