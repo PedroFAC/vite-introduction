a3b76764141437f000d963a152393e3b19d6dcc9
Pedro Façanha <pedrofacpeix@gmail.com>
2023-10-27 10:30:33 -0300

    env variables

--- static-server/main.js
+++ static-server/main.js
@@ -1,5 +1,10 @@
 import classes from "./style.module.css";
 
 console.log("Hello World!");
+console.log("Server is running in " + import.meta.env.MODE);
+console.log(import.meta.env);
+if (import.meta.env.DEV) {
+  console.log("This is Dev Server");
+}
 
 document.querySelector("h1").className = classes.title;
