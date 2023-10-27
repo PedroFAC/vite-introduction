02db453b80d77edf7e1d09e4bcfbb0e9edcdf79c
Pedro Façanha <pedrofacpeix@gmail.com>
2023-10-27 14:59:40 -0300

    envPrefix

--- vite-config/vite.config.js
+++ vite-config/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig((command, mode) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_");
-
-  console.log(env);
-
-  return {};
+export default defineConfig({
+  clearScreen: false,
+  envPrefix: "APP_",
 });
